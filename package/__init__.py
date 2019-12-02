0302c02ab4e4a4006bb529d9ac89a70587705206
Dr.-Ing. Peter Kötzing <peter.koetzing@vattenfall.de>
2019-12-02 12:09:54 +0100

    add comment

--- package/__init__.py
+++ package/__init__.py
@@ -3,4 +3,7 @@ logger = logging.getLogger(__name__)
 
 logger.info('init code')
 
+# convenience import:
 from package.helppackage.helpmodule import helper
+# helper is added to the main package namespace and can be imported as
+# from package import helper
